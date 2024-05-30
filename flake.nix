ce237ea48979517509070fb96b21861314d58e7e
ThetaSinner <ThetaSinner@no-reply.github.com>
2024-05-30 18:27:26 +0100

    Update version and add tryorama input

--- flake.nix
+++ flake.nix
@@ -1,23 +1,26 @@
 {
   inputs = {
-    versions.url = "github:holochain/holochain?dir=versions/0_2_rc";
+    versions.url = "github:holochain/holochain?dir=versions/0_3_rc";
     
-    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.5-rc.1";
-    versions.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.4.1";
+    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.3.1-rc.0";
+    versions.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.4.4";
 
     holochain = {
       url = "github:holochain/holochain";
       inputs.versions.follows = "versions";
-    };    
+    };
+
+    tryorama.url = "github:holochain/tryorama/main"; # currently at 0.3
   };
 
-  outputs = { self, nixpkgs, versions, holochain }: {
+  outputs = { self, nixpkgs, versions, holochain, tryorama }: {
     nixosConfigurations.nuc = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
       specialArgs = { 
         lair-keystore = holochain.packages.x86_64-linux.lair-keystore;
         holochain = holochain.packages.x86_64-linux.holochain;
+        trycp-server = tryorama.packages.x86_64-linux.trycp-server;
       };
     };
   };
